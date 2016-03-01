58e731d2f24330db4d019e05b0a51152123c06d7
Visraant <visraant@gmail.com>
2016-02-29 20:16:48 -0600

    initial commit

---
+++ .store_item.rb
@@ -0,0 +1,7 @@
+item1 = {:name => "nerf gun", :color => "red", :price => "30.00"}
+item2 = {name: "super soaker", color: "blue", price: "25.00"}
+item3 = {:name => "laser sword", :color => "green", :price => "20.00"}
+
+puts "#{item1[:name]} is #{item1[:color]} and costs #{item1[:price]}."
+puts "#{item2[:name]} is #{item2[:color]} and costs #{item2[:price]}."
+puts "#{item3[:name]} is #{item3[:color]} and costs #{item3[:price]}."
